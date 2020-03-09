7c914bd0dc77bf05450e5ad0123adaa576985986
Alex <alextheprogrammer21@gmail.com>
2020-03-09 03:50:11 +0000

    finished with bug fixes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,24 @@
+//---------------------------- Helper Functions------------------------
 function generateRandomName() {
   return (Math.random().toString(36).substring(2, 15)).slice(0,6);
-
-};
+}
 
 let obj = {};
-  let counter = 1;
+let counter = 1;
 
 $(document).ready(() => {
   $('main .error').slideUp();
   $('main compose').slideUp();
 
-
-  
-
-
   //-------------------------------Nav bar buttons-------------------------
 
   $("nav new-tweet img").hover(function() {
     $(this).css("margin-top", "0%");
-
   }, function() {
     $(this).css("margin-top", "-10%");
   });
 
-
   $('nav new-tweet').click(() => {
-    // $('main section').animate({opacity: '100%'})
     $('main compose').slideToggle("slow");
     $('main textarea').focus();
   });
@@ -36,15 +29,15 @@ $(document).ready(() => {
   };
   
   const $loadtweets = () => {
-    // return $.ajax('/tweets', { method: 'GET' })
     return hitServer('GET');
   };
 
-  $('#tweet-input-form').submit((event) => {  //START OF SUBMIT BUTTON CLICK
+  //----------------------------Submit form---------------------------
+  $('#tweet-input-form').submit((event) => {
     event.preventDefault();
 
     let $textValue = $('main textarea')[0].value;
-    //------------------------------Submitting New Tweets-----------------------
+
     if ($textValue.length < 1) {
       $('main .error').slideDown();
         
@@ -54,7 +47,6 @@ $(document).ready(() => {
 
       return null;
     }
-
     if ($textValue.length > 140) {
       $('main .error').slideDown();
 
@@ -68,35 +60,18 @@ $(document).ready(() => {
     $('main textarea')[0].value = "";
     $('.counter').text(140);
 
-    // $(".tweet-container article").hover(function() {
-    //   $(".tweet-container div2 img").css("opacity", "100%");
-    // }, function() {
-    //   $(".tweet-container div2 img").css("opacity", "0%");
-    // });
-
     //-------------------------------Rendering New Tweets----------------------------
-
-  
 
     const safeHTML = `<p>${escape($textValue)}</p>`;
 
-    // $.ajax('/tweets', { method: 'GET' })
-    // .then((response) => {
-      let handle = "@" + generateRandomName();
+    let handle = "@" + generateRandomName();
     obj[counter] = [{"user": {avatars: "https://i.imgur.com/73hZDYK.png",
       handle: handle,
       name:   generateRandomName()},
-                      
     "content": {text: safeHTML},
-                
     "created_at": "today" }];
-
-    // $loadtweets().then(renderTweets);
-      counter++;
-    renderTweets(obj[counter-1]);
-      
-    // })
-
+    counter++;
+    renderTweets(obj[counter - 1]);
   });
 
   const renderTweets = function(tweets) {
@@ -114,7 +89,6 @@ $(document).ready(() => {
      <header>
          <img src="${tweet.user.avatars}" />
          ${tweet.user.name}
-       
        <span>${tweet.user.handle}</span>
      </header>
      <p>${tweet.content.text}</p>
@@ -124,17 +98,11 @@ $(document).ready(() => {
      </footer>
     </article>
    `;
-
     return $($tweet);
   };
-  
 });
 
-
-//------------------------Helper Functions-------------------------------
-
 const escape =  function(str) {
-
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
--- public/scripts/compose-character-counter.js
+++ public/scripts/compose-character-counter.js
@@ -1,23 +1,18 @@
-$(document).ready( () => {
+$(document).ready(() => {
   
+  //------------------- 140 Character counter functionality -------------------//
   const $inputForm = $('main #tweet-input-form textarea');
-  let $countValue = $('.counter')[0].textContent;
   
   $inputForm.on("keyup", (event) => {
-    
-    let $inputLength = event.target.textLength;
-    
+    let inputLength = event.target.textLength;
+    $('.counter').text(140 - inputLength);
 
-    $('.counter').text(140 - $inputLength);
-
-    
-    if ($inputLength > 140) {
+    if (inputLength > 140) {
       $('.counter').css("color", "red");
     }
 
-    if ($inputLength <= 140) {
+    if (inputLength <= 140) {
       $('.counter').css("color", "black");
     }
   });
-
 });
